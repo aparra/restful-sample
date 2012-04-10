2d1e3781086da40405c168c7cd502bb186f36441
Marcos Lemos <mvslemos@gmail.com>
2012-04-10 00:20:47 -0300

    criando servico de login e alteracao de dados.

--- src/main/java/br/com/amil/restful/service/UsuarioRest.java
+++ src/main/java/br/com/amil/restful/service/UsuarioRest.java
@@ -8,8 +8,10 @@ import br.com.amil.model.Usuario;
 import br.com.amil.restful.mediatype.usuario.UsuarioMediaType;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Status;
 
 @Resource
 @Path("/servicos/usuario")
@@ -17,18 +19,38 @@ public class UsuarioRest {
 
 	private final Result result;
 	
-	private UsuarioRest(Result result) {
+	private Usuario usuario;
+	
+	private Status status;
+	
+	private UsuarioRest(Result result, Status status) {
 		this.result = result;
-	}
-
-	@Get("")
-	public void usuario() {
+		this.status = status;
 		Usuario usuario = new Usuario();
 		usuario.setLogin("meu_login");
 		usuario.setSenha("123qwe");
 		usuario.setUltimoAcesso(new Date());
-		
-		result.use(representation()).from(new UsuarioMediaType(usuario)).recursive().serialize();
+		this.usuario = usuario;
+	}
+
+	@Get("")
+	public void usuario() {
+		result.use(representation()).from(new UsuarioMediaType(this.usuario)).recursive().serialize();
 	}
 	
+	@Post("")
+	public void usuario(Usuario usuario) {
+		this.usuario.setLogin(usuario.getLogin());
+		this.usuario.setSenha(usuario.getSenha());
+		result.use(representation()).from(new UsuarioMediaType(this.usuario)).recursive().serialize();
+	}
+
+	@Post("/login")
+	public void login(Usuario usuario) {
+		if (this.usuario.getLogin().equals(usuario.getLogin()) && this.usuario.getSenha().equals(usuario.getSenha())) {
+			this.status.accepted();
+		} else {
+			this.status.forbidden("Usuário ou senha inválido.");
+		}
+	}
 }
